a80e85d1a6365599b487e9eea8916fc1b129e9d2
AElalfee <abdelrahman@elalfee.com>
2025-06-24 12:53:18 +0400

    date format helper

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ from datetime import datetime
 
 def now_iso():
     return datetime.now().isoformat()
+
+
+def date_format(iso_date):
+    date_format = "%d %b %Y at %H:%M:%S"
+    date_object = datetime.fromisoformat(iso_date)
+    return date_object.strftime(date_format)
