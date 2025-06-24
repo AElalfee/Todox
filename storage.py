857e88a8f7290ddadddf1a00837811c730cfeb97
AElalfee <abdelrahman@elalfee.com>
2025-06-24 14:06:13 +0400

    handle archive json loading/saving

--- storage.py
+++ storage.py
@@ -2,6 +2,7 @@ import json
 import os
 
 FILE_NAME = "tasks.json"
+ARCHIVE_FILE = "archive.json"
 
 
 def load_tasks():
@@ -18,4 +19,21 @@ def load_tasks():
 
 def save_tasks(tasks):
     with open(FILE_NAME, "w") as f:
+        json.dump(tasks, f, indent=4)
+
+
+def load_archived_tasks():
+    if not os.path.exists(ARCHIVE_FILE):
+        with open(ARCHIVE_FILE, "w") as f:
+            json.dump([], f)
+    with open(ARCHIVE_FILE, "r") as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError as e:
+            print(f"Error: {e}")
+            return []
+
+
+def save_archived_tasks(tasks):
+    with open(ARCHIVE_FILE, "w") as f:
         json.dump(tasks, f, indent=4)
